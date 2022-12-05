28673d9ec4200dbe9555c6ccb1a01814f93631d1
GP8r <36450853+gasparfalvip@users.noreply.github.com>
2022-12-05 17:55:26 +0100

    Add files via upload

---
+++ WUWIOS_1130/JSONParseWUWIOS/src/WUWIOS/ListWUWIOS.java
@@ -0,0 +1,26 @@
+package WUWIOS;
+
+import java.io.File;
+
+import org.json.*;
+import org.json.simple.JSONArray;  
+
+public class ListWUWIOS {
+
+	public static void main(String[] args) {
+		
+		JSONArray a = new JSONArray();
+		
+		a.put(0, "BL");
+		a.put(1, "100000.0");
+		a.put(2, "21");
+		
+		System.out.println(a);
+		
+		
+				
+		
+		
+	}
+
+}
